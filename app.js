dc5401fa46261b69aa8d92e475def6b9bd17fe80
パン粉 ライ麦 <relipmoc101@gmail.com>
2017-11-28 21:38:39 +0900

    uhehe

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ console.log('Server running!');
 
 // リクエストの処理
 function doRequest(req, res) {
-    fs.readFile('test.html','shift-jis',
+    fs.readFile('test.html','UTF-8',
     function(err,data){
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
